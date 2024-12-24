25f0930c1dea9d57cfc91943467864dd28aafeb5
S K <skohari@gmail.com>
2024-12-24 02:56:25 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Specify the output directory
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
