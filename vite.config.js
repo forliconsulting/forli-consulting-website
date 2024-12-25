414f4f4c9fd1fbe61b13d6bc3b9eb94b18074b9d
S K <skohari@gmail.com>
2024-12-25 17:06:03 -0600

    try double quotes

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/forli-consulting-website/', // Ensure the base path is set correctly
+  base: "/forli-consulting-website/", // Ensure the base path is set correctly
 });
