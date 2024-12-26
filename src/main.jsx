be22f9322ea3a31f5722cda6dbb72e3eb9d6a38b
S K <skohari@gmail.com>
2024-12-26 00:23:33 -0600

    not working

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// filepath: /c:/Users/fc12/Documents/code/forli-consulting-website/forli-consulting-website/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
